3bdf0b3b34986d8fb925f306dc8ed0c565bfb5ed
Usama Ejaz <osamaejaz1@gmail.com>
2018-01-13 22:54:37 +0500

    add freeter

---
+++ freeter/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://freeter.io/download/FreeterSetup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'Freeter*'
+
+  checksum      = '4ED2430FA43617C9E987CB0F0C40B13527609FBE4FD55BAC0576470BEE4C551B'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
